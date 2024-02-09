e2fd76e851ad9b1279859127266abaa21bf7d1f9
JelaJs <nikola4434@gmail.com>
2024-02-09 19:31:08 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <div class="logo">
+      <h1>Logo</h1>
+    </div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/explore">Find Hotel</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style scoped lang="scss">
+header {
+  max-width: 130rem;
+  margin: 0 auto;
+  padding: 1rem 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: transparent;
+
+  nav {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+  }
+}
+</style>
---
+++ src/views/FindHotelView.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div class="header-input-wrap">
+      <h1>Find hotel page</h1>
+      <!--<input id="autocomplete" type="text" placeholder="Search for your favorite palce..." />-->
+      <GoogleAddressAutocomplete
+        apiKey="AIzaSyCp2tWmiWmxkqnw_nkzmTI51J6X8Wwn_ug"
+        v-model="address"
+        @callback="getLatLng"
+        class="css-class-here"
+        placeholder="placeholder if you wish"
+      />
+    </div>
+    <div class="flex">
+      <div class="menu">
+        <ul>
+          <li><div>Hotel Info</div></li>
+          <li><div>Hotel Info</div></li>
+          <li><div>Hotel Info</div></li>
+          <li><div>Hotel Info</div></li>
+        </ul>
+      </div>
+      <div id="map"></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+/* eslint-disable no-undef */
+import { ref, onMounted } from 'vue'
+import GoogleAddressAutocomplete from 'vue3-google-address-autocomplete'
+
+const address = ref('')
+
+const getLatLng = (place) => {
+  const lat = place.geometry.location.lat()
+  const lng = place.geometry.location.lng()
+  console.log(lat, lng)
+}
+
+onMounted(() => {})
+</script>
+
+<style scoped lang="scss">
+.header-input-wrap {
+  padding-left: 1rem;
+  input {
+    outline: none;
+    padding: 0.5rem 0.5rem;
+    width: 25rem;
+  }
+}
+.flex {
+  display: flex;
+  margin-top: 3rem;
+
+  .menu {
+    padding-left: 1rem;
+    width: 30%;
+    height: 100%;
+  }
+
+  .map {
+    width: 70%;
+    height: 100%;
+  }
+}
+</style>
