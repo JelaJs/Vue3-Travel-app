fd0387a6852f1c1580392f5b1b09a88f498ef7ab
JelaJs <nikola4434@gmail.com>
2024-02-10 16:32:26 +0100

    Autocomplete and google map load on search implemented

--- src/views/FindHotelView.vue
+++ src/views/FindHotelView.vue
@@ -2,14 +2,14 @@
   <div>
     <div class="header-input-wrap">
       <h1>Find hotel page</h1>
-      <!--<input id="autocomplete" type="text" placeholder="Search for your favorite palce..." />-->
-      <GoogleAddressAutocomplete
+      <input id="autocomplete" type="text" placeholder="Search for your favorite palce..." />
+      <!--<GoogleAddressAutocomplete
         apiKey="AIzaSyCp2tWmiWmxkqnw_nkzmTI51J6X8Wwn_ug"
         v-model="address"
         @callback="getLatLng"
         class="css-class-here"
         placeholder="placeholder if you wish"
-      />
+      />-->
     </div>
     <div class="flex">
       <div class="menu">
@@ -27,18 +27,37 @@
 
 <script setup>
 /* eslint-disable no-undef */
-import { ref, onMounted } from 'vue'
-import GoogleAddressAutocomplete from 'vue3-google-address-autocomplete'
+import { onMounted } from 'vue'
+//import GoogleAddressAutocomplete from 'vue3-google-address-autocomplete'
 
-const address = ref('')
+//const address = ref('')
+//const lat = ref(0)
+//const lng = ref(0)
 
-const getLatLng = (place) => {
-  const lat = place.geometry.location.lat()
-  const lng = place.geometry.location.lng()
+/*const getLatLng = (place) => {
+  lat.value = place.geometry.location.lat()
+  lng.value = place.geometry.location.lng()
   console.log(lat, lng)
+}*/
+
+const showMap = (lat, lng) => {
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: new google.maps.LatLng(lat, lng)
+    //mapTypeId: google.maps.mapTypeId.ROADMAP
+  })
 }
 
-onMounted(() => {})
+onMounted(() => {
+  let autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'))
+  autocomplete.addListener('place_changed', () => {
+    let place = autocomplete.getPlace()
+    const lat = place.geometry.location.lat()
+    const lng = place.geometry.location.lng()
+
+    showMap(lat, lng)
+  })
+})
 </script>
 
 <style scoped lang="scss">
@@ -56,13 +75,13 @@ onMounted(() => {})
 
   .menu {
     padding-left: 1rem;
-    width: 30%;
+    width: 20%;
     height: 100%;
   }
 
-  .map {
-    width: 70%;
-    height: 100%;
+  #map {
+    width: 80%;
+    height: 100vh;
   }
 }
 </style>
